06737a786fd615286924a8f6f148e3b434fc101d
Sarthak-shastrakar-88 <sarthakshastrakar9@gmail.com>
2024-12-20 07:41:28 +0530

    DEPLOY FRONTEND

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "https://videoconferencingbackend-69w7.onrender.com"
+  ? "https://video-conferencing-app-backend.onrender.com  "
   : "http://localhost:8001";
 
 export default server;
